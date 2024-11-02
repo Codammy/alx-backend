d76dd53376b395fed49d94ae84aff22a3c71f2cc
codam <damisco005@gmail.com>
2024-11-02 13:37:08 +0100

    corrected LIFOCaching

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -11,14 +11,16 @@ class LIFOCache(base_caching.BaseCaching):
     def __init__(self):
         """class initializer"""
         super().__init__()
+        self.last_mod = None
 
     def put(self, key, item):
         """assign to the dictionary self.cache_data"""
         if key and item:
-            if not self.cache_data.get(key) and len(self.cache_data) >= self.MAX_ITEMS:
-                to_pop = self.cache_data.popitem()
-                print(f'DISCARD {to_pop[0]}')
+            if not self.get(key) and len(self.cache_data) >= self.MAX_ITEMS:
+                self.cache_data.pop(self.last_mod)
+                print(f'DISCARD {self.last_mod}')
             self.cache_data[key] = item
+            self.last_mod = key
 
     def get(self, key):
         """return the value in self.cache_data linked to key"""
