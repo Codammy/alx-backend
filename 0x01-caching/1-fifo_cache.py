ddaf94f20ee60841853593d97bd7f7feffad1b8f
codam <damisco005@gmail.com>
2024-11-02 13:02:19 +0100

    corrected FIFOCaching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -15,14 +15,13 @@ class FIFOCache(base_caching.BaseCaching):
     def put(self, key, item):
         """assign to the dictionary self.cache_data"""
         if key and item:
+            if not self.get(key) and len(self.cache_data) >= self.MAX_ITEMS:
+                to_pop = list(self.cache_data.keys())[0]
+                print(f'DISCARD {to_pop}')
+                self.cache_data.pop(to_pop)
             self.cache_data[key] = item
-            if len(self.cache_data) > self.MAX_ITEMS:
-                to_pop = list(self.cache_data.keys())
-                print(f'DISCARD {to_pop[0]}')
-                self.cache_data.popitem()
 
     def get(self, key):
         """return the value in self.cache_data linked to key"""
         if key:
             return self.cache_data.get(key, None)
-        
