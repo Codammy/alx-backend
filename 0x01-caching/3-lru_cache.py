60120ac20b0665e7f38d17872f03c765ff9cca75
codam <damisco005@gmail.com>
2024-11-03 07:46:40 +0100

    implemented MRU and LRU caching system

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,28 +1,49 @@
 #!/usr/bin/env python3
-"""FIFOCache that inherits from BaseCaching and is a caching system
+"""LRUCache that inherits from BaseCaching and is a caching system
 """
 
 import base_caching
 
 
-class FIFOCache(base_caching.BaseCaching):
-    """FIFOCache that inherits from BaseCaching"""
+class LRUCache(base_caching.BaseCaching):
+    """Least Recently Used(LRU) Cache that inherits from BaseCaching"""
 
     def __init__(self):
         """class initializer"""
         super().__init__()
+        self.__rank = {}
+        self.access_times = 0
 
     def put(self, key, item):
         """assign to the dictionary self.cache_data"""
         if key and item:
+            if not self.get(key) and len(self.cache_data) >= self.MAX_ITEMS:
+                least_accessed = self.__least_accessed()[0]
+                self.cache_data.pop(least_accessed)
+                print(f'DISCARD: {least_accessed}')
+
+            if len(self.cache_data) == 0:
+                self.__rank[key] = 0
+            else:
+                self.__rank[key] = self.access_times
+
             self.cache_data[key] = item
-            if len(self.cache_data) > self.MAX_ITEMS:
-                to_pop = list(self.cache_data.keys())
-                print(f'DISCARD {to_pop[0]}')
-                self.cache_data.popitem()
+            self.access_times += 1
+
+    def __least_accessed(self):
+        """returns the key with the least accessed times."""
+        least_accessed = (None, self.access_times)
+        for k, t in self.__rank.items():
+            if t < least_accessed[1]:
+                least_accessed = (k, t)
+        self.__rank.pop(least_accessed[0])
+        return least_accessed
 
     def get(self, key):
         """return the value in self.cache_data linked to key"""
         if key:
-            return self.cache_data.get(key, None)
-        
+            item = self.cache_data.get(key, None)
+            if item:
+                self.access_times += 1
+                self.__rank[key] = self.access_times
+            return item
--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,28 +1,33 @@
 #!/usr/bin/env python3
-"""FIFOCache that inherits from BaseCaching and is a caching system
+"""MRUCache that inherits from BaseCaching and is a caching system
 """
 
 import base_caching
 
 
-class FIFOCache(base_caching.BaseCaching):
-    """FIFOCache that inherits from BaseCaching"""
+class MRUCache(base_caching.BaseCaching):
+    """Most Recently Used(MRU) Cache that inherits from BaseCaching"""
 
     def __init__(self):
         """class initializer"""
         super().__init__()
+        self.most_accessed_key = None
 
     def put(self, key, item):
         """assign to the dictionary self.cache_data"""
         if key and item:
+            if not self.get(key) and len(self.cache_data) >= self.MAX_ITEMS:
+                most_accessed = self.most_accessed_key
+                self.cache_data.pop(most_accessed)
+                print(f'DISCARD: {most_accessed}')
+
             self.cache_data[key] = item
-            if len(self.cache_data) > self.MAX_ITEMS:
-                to_pop = list(self.cache_data.keys())
-                print(f'DISCARD {to_pop[0]}')
-                self.cache_data.popitem()
+            self.most_accessed_key = key
 
     def get(self, key):
         """return the value in self.cache_data linked to key"""
         if key:
-            return self.cache_data.get(key, None)
-        
+            item = self.cache_data.get(key, None)
+            if item:
+                self.most_accessed_key = key
+            return item
