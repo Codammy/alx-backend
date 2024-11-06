d25611aeac0ca486ac97973d73b069689ff98386
codam <damisco005@gmail.com>
2024-11-06 05:55:17 +0100

    new commit

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -5,18 +5,19 @@ from flask_babel import Babel
 import typing
 
 
-app = Flask(__name__)
-app.config.from_object('config.Config')
-babel = Babel(app)
-
-
-@babel.localselector
 def get_locale():
     """returns language locale"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+app = Flask(__name__)
+app.config.from_object('config.Config')
+babel = Babel(app, locale_selector=get_locale)
 
 
 @app.route('/')
 def index_page() -> typing.Any:
     """renders index page"""
     return render_template('index.html')
+
+app.run()
